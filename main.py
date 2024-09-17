a76b61bc397ea86d9ae16c69c64276629f769650
unknown <arcsmo19@gmail.com>
2024-09-17 23:04:42 +0530

    fix: main.py fixes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from SECedgarpyExtractor import CIKExtractor
-import SECedgarpyProcessing 
-import SECedgarpyDownloading 
+from SECedgarpyProcessing import extract10Kurl, URLtoXLSX
+from SECedgarpyDownloading import getXLSXfile, GenerateCSVreport
 
 
 #Extracting the CIK numbers for the first 500 companies on the NYSE
@@ -8,9 +8,13 @@ companies = CIKExtractor()
 print("Extracted first 500 companies on the NYSE")
 
 #Extracting data from the website and placing it in a XLSX file
-data = SECedgarpyProcessing(companies)
+urls = list()
+for company in companies:
+    data = extract10Kurl(company)
+    urls.append(data)
 print("Data extracted")
+file = URLtoXLSX(urls)
 
 #Converting the XLSX file to  CSV file
-SECedgarpyDownloading(data)
+GenerateCSVreport(getXLSXfile(file))
 print("Required CSV file in #location")
